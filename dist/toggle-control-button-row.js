ac20ce8f045ce2cce4a2724441ff4061196d92dd
finity69x2 <32221243+finity69x2@users.noreply.github.com>
2023-05-07 01:10:55 -0400

    Update toggle-control-button-row.js

--- dist/toggle-control-button-row.js
+++ dist/toggle-control-button-row.js
@@ -6,12 +6,41 @@ window.customCards.push({
   preview: false,
 });
 
-class CustomToggleRow extends Polymer.Element {
+const LitElement = customElements.get("ha-panel-lovelace") ? Object.getPrototypeOf(customElements.get("ha-panel-lovelace")) : Object.getPrototypeOf(customElements.get("hc-lovelace"));
+const html = LitElement.prototype.html;
+const css = LitElement.prototype.css;
 
-	static get template() {
-		return Polymer.html`
-		<style is="custom-style" include="iron-flex iron-flex-alignment"></style>
-		<style>
+class CustomToggleRow extends LitElement {
+
+	constructor() {
+		super();
+		this._config = {
+			customTheme: false,
+			width: '30px',
+			height: '30px',
+			isOnColor: '#43A047',
+			isOffColor: '#f44c09',
+			customOffText: 'OFF',
+			customOnText: 'ON',
+		};
+	}
+	
+	static get properties() {
+		return {
+			hass: Object,
+			_config: Object,
+			_stateObj: Object,
+			_width: String,
+			_height: String,
+			_buttonColor: String,
+			_buttonText: String,
+			_buttonName: String,
+			_buttonState: Boolean,
+		};
+	}
+	
+	static get styles() {
+		return css`
 			:host {
 				line-height: inherit;
 			}
@@ -28,57 +57,41 @@ class CustomToggleRow extends Polymer.Element {
 				padding: 1px;
 				cursor: pointer;
 			}
-		</style>
-		<hui-generic-entity-row hass="[[hass]]" config="[[_config]]">
-			<div class='horizontal justified layout' on-click="stopPropagation">
-				<button
-					class='switch'
-					style='[[_buttonColor]];min-width:[[_width]];max-width:[[_width]];height:[[_height]]'
-					toggles name='[[_buttonName]]'
-					on-click='setState'>
-						[[_buttonText]]
-				</button>
-			</div>
-		</hui-generic-entity-row>
 		`;
 	}
-
-	static get properties() {
-		return {
-			hass: {
-				type: Object,
-				observer: 'hassChanged'
-			},
-			_config: Object,
-			_stateObj: Object,
-			_width: String,
-			_height: String,
-			_buttonColor: String,
-			_buttonText: String,
-			_buttonName: String,
-			_buttonState: Boolean,
-	        }
-    	}
-
-	setConfig(config) {
-		this._config = config;
-
-		this._config = {
-			customTheme: false,
-			width: '30px',
-			height: '30px',
-			isOnColor: '#43A047',
-			isOffColor: '#f44c09',
-			customOffText: 'OFF',
-			customOnText: 'ON',
-			...config
-		};
+	
+	render() {
+		return html`
+			<hui-generic-entity-row .hass="${this.hass}" .config="${this._config}">
+				<div id='button-container' class='horizontal justified layout'>
+					<button
+						class='switch'
+						style='${this._buttonColor};min-width:${this._width};max-width:${this._width};height:${this._height}'
+						toggles name="${this._buttonName}"
+						@click=${this.setState}>${this._buttonText}</button>
+				</div>
+			</hui-generic-entity-row>
+		`;
+	}
+	
+	firstUpdated() {
+		super.firstUpdated();
+		this.shadowRoot.getElementById('button-container').addEventListener('click', (ev) => ev.stopPropagation());
 	}
 
-	hassChanged(hass) {
+	setConfig(config) {
+		this._config = { ...this._config, ...config };
+	}
 
+	updated(changedProperties) {
+		if (changedProperties.has("hass")) {
+			this.hassChanged();
+		}
+	}
+	
+	hassChanged() {
 		const config = this._config;
-		const stateObj = hass.states[config.entity];
+		const stateObj = this.hass.states[config.entity];
 		const custTheme = config.customTheme;
 		const buttonWidth = config.width;
 		const buttonHeight = config.height;
@@ -86,7 +99,6 @@ class CustomToggleRow extends Polymer.Element {
 		const custOffClr = config.isOffColor;
 		const custOffTxt = config.customOffText;
 		const custOnTxt = config.customOnText;
-	
 		
 		let state;
 			if (stateObj) {
@@ -125,43 +137,32 @@ class CustomToggleRow extends Polymer.Element {
 		let unavailname = 'unavailable';
 	
 		if (state == 'off') {
-			this.setProperties({
-			_stateObj: stateObj,
-			_buttonState: state,
-			_buttonName: onname,
-			_width: buttonwidth,
-			_height: buttonheight,
-			_buttonColor: color,
-			_buttonText: offtext,
-			});
+			this._stateObj = stateObj;
+			this._buttonState = state;
+			this._buttonName = onname;
+			this._width = buttonwidth;
+			this._height = buttonheight;
+			this._buttonColor = color;
+			this._buttonText = offtext;
 		} else if (state == 'on') {
-			this.setProperties({
-			_stateObj: stateObj,
-			_buttonState: state,
-			_width: buttonwidth,
-			_height: buttonheight,
-			_buttonName: offname,
-			_buttonColor: color,
-			_buttonText: ontext,
-			});
+			this._stateObj = stateObj;
+			this._buttonState = state;
+			this._width = buttonwidth;
+			this._height = buttonheight;
+			this._buttonName = offname;
+			this._buttonColor = color;
+			this._buttonText = ontext;
 		} else {
-			this.setProperties({
-			_stateObj: stateObj,
-			_buttonState: state,
-			_buttonName: unavailname,
-			_width: buttonwidth,
-			_height: buttonheight,
-			_buttonColor: color,
-			_buttonText: unavailtext,
-			});
+			this._stateObj = stateObj;
+			this._buttonState = state;
+			this._buttonName = unavailname;
+			this._width = buttonwidth;
+			this._height = buttonheight;
+			this._buttonColor = color;
+			this._buttonText = unavailtext;
 		}
 	}
-	
-	
-	stopPropagation(e) {
-		e.stopPropagation();
-	}
-
+		
 	setState(e) {
 		const state = e.currentTarget.getAttribute('name');
 		if( state == 'on' ){
